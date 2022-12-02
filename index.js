ac6f4fd61391ca2f698cb6f2bf19294c835ef288
ZAMANXD <znsnlab@gmail.com>
2022-12-02 23:40:30 +0600

    minor update

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const client = new MongoClient(uri, {
 // client.connect((err) => {
 //   const collection = client.db('test').collection('devices');
 //   // perform actions on the collection object
-//   client.close();
 
 // console.log(uri)
 
